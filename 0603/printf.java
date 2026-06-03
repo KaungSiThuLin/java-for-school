53e9fdfa67c7133831a78cab1298b9a53f3e77df
KaungSiThuLin <2505049@i-seifu.jp>
2026-06-03 10:02:47 +0900

    update

---
+++ 0603/printf.java
@@ -0,0 +1,8 @@
+public class printf {
+    public static void main(String[] args) {
+        int a =100;
+        // double b = 3.14;
+        // String c = "Java";
+        System.out.printf("%d", a); //, b, c);
+    }
+}
